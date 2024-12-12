340c61f40bef7c90e84ba284d0af820401ede5e5
GenaiHackathonSA <genaihackathonsa@gmail.com>
2024-12-12 14:50:53 +0530

    feat: commit for T1-24

--- backend/src/main/java/com/fullStack/expenseTracker/controllers/CategoryController.java
+++ backend/src/main/java/com/fullStack/expenseTracker/controllers/CategoryController.java
@@ -1,36 +1,30 @@
 package com.fullStack.expenseTracker.controllers;
 
+import com.fullStack.expenseTracker.dto.ApiResponseDto;
 import com.fullStack.expenseTracker.services.CategoryService;
-import com.fullStack.expenseTracker.dto.reponses.ApiResponseDto;
-import com.fullStack.expenseTracker.exceptions.CategoryNotFoundException;
-import com.fullStack.expenseTracker.exceptions.CategoryServiceLogicException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
+import com.fullStack.expenseTracker.exception.CategoryServiceLogicException;
+import com.fullStack.expenseTracker.exception.CategoryNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*")
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/spendwise/category")
 public class CategoryController {
 
-    @Autowired
-    private CategoryService categoryService;
+    private final CategoryService categoryService;
 
-    @GetMapping("/getAll")
-    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
-    public ResponseEntity<ApiResponseDto<?>> getAllCategories() {
-        return categoryService.getCategories();
+    // Constructor for dependency injection
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
     }
 
     @DeleteMapping("/delete")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ResponseEntity<ApiResponseDto<?>> disableOrEnableCategory(@Param ("categoryId") int categoryId)
+    public ResponseEntity<ApiResponseDto<?>> toggleCategoryStatus(@RequestParam("categoryId") int categoryId)
             throws CategoryServiceLogicException, CategoryNotFoundException {
         return categoryService.enableOrDisableCategory(categoryId);
     }
-
 }
-
-
